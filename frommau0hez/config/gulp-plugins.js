1e3174d6d51cf3120dc60c8431e8bb393fc641bf
Mau0HeZzz <mau0hezzz@hotmail.com>
2022-06-01 19:19:58 +0300

    initial mau0hezzz

---
+++ frommau0hez/config/gulp-plugins.js
@@ -0,0 +1,19 @@
+// Импортируем модули
+import notify from "gulp-notify";
+import newer from "gulp-newer";
+import plumber from "gulp-plumber";
+import ifPlugin from "gulp-if";
+import prettier from "gulp-prettier";
+import rename from 'gulp-rename';
+import fileinclude from 'gulp-file-include';
+
+// Экспортируем объект
+export const plugins = {
+	notify,
+	if: ifPlugin,
+	prettier,
+	newer,
+	plumber,
+	rename,
+	fileinclude
+}
